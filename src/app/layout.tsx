9ea22bb6a8121ac8b0ffcda66a6f190fe564134c
CodeWander-666 <nikhil.singh.study999@gmail.com>
2026-07-01 16:22:26 +0530

    ✨ Add luxury golden B favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         {/* Google Analytics Tag */}
         <Script
           async
